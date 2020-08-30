6305dfa68cebad72435609f7740f8daaf4e1b640
a84707227 <84707227@qq.com>
2020-08-30 21:33:59 +0800

    测试MVC方式

--- springboot-web/src/main/java/com/test/controller/HomeController.java
+++ springboot-web/src/main/java/com/test/controller/HomeController.java
@@ -1,20 +1,56 @@
 package com.test.controller;
 
+import com.test.pojo.Home;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-//    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
     public String hello(){
         System.out.println("hello world");
         System.out.println("welcome从 ");
         System.out.println("test");
         return "home";
     }
+
+    @RequestMapping(value = "/homeList",method = RequestMethod.GET)
+    @ResponseBody
+    public String findHomeListByUrl(@RequestParam("pageSize") int pageSize,@RequestParam("pageNum") int pageNum){
+        System.out.println("每页大小：" + pageSize + ";当前页：" + pageNum);
+        return "findHomeListByUrl";
+    }
+
+    @RequestMapping(value = "/homeList2",method = RequestMethod.GET)
+    @ResponseBody
+    public String findHomeListByUrl2(@RequestParam(value = "pageSize",defaultValue = "15") int pageSize,@RequestParam("pageNum") int pageNum){
+        System.out.println("每页大小：" + pageSize + ";当前页：" + pageNum);
+        return "findHomeListByUrl";
+    }
+
+    @RequestMapping(value = "/getHome/{homeId}/{homeName}",method = RequestMethod.GET)
+    @ResponseBody
+    public String getHome(@PathVariable("homeId") String homeId,@PathVariable String homeName){
+        System.out.println("Home的ID是：" + homeId + "Home的名称：" + homeName);
+        return "getHome";
+    }
+
+    @RequestMapping(value = "saveHome", method = RequestMethod.POST)
+    @ResponseBody
+    public Object saveHome(@Valid Home home, Errors errors){
+        if (errors.hasErrors()){
+            System.out.println("错误内容："+errors.getFieldErrors());
+            return errors;
+        }
+        System.out.println(home);
+        return "home";
+    }
+
 
 }
